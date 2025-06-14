b694d4180d6f750c36757fa9c440507faa097618
MAnjeet <manjeetmaurya7785@gmail.com>
2025-06-15 00:00:55 +0530

    first commit

---
+++ bionmial.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class bionmial {
+
+    public static int factorial(int n) {
+        int f = 1;
+        for (int i = 1; i <= n; i++) {
+            int factoriall = f * i;
+        }
+        return f;
+    }
+
+    public static void bionmialCoff(int n, int r) {
+        int a = factorial(n);
+
+    }
+}
