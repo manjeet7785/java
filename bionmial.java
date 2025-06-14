52c010db84550e56c062f86bc1bff27606490ff1
manjeet7785 <manjeetmaurya7785@gmail.com>
2025-06-15 00:16:05 +0530

    Update bionmial.java
    how to find binomial coff

--- bionmial.java
+++ bionmial.java
@@ -1,18 +1,27 @@
 
 import java.util.*;
-
 public class bionmial {
 
     public static int factorial(int n) {
         int f = 1;
         for (int i = 1; i <= n; i++) {
-            int factoriall = f * i;
+            f = f * i;
         }
         return f;
     }
 
-    public static void bionmialCoff(int n, int r) {
-        int a = factorial(n);
+    public static int bionmialCoff(int n, int r) {
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n - r);
+        int bionmialCoff = fact_n / (fact_r * fact_nmr);
+        return bionmialCoff;
 
     }
+
+    public static void main(String[] args) {
+        int res = bionmialCoff(5, 2);
+        System.out.println(res);
+    }
+
 }
